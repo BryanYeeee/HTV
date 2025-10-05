72bca44f2fd2ecfdf2f3eb1c1acaecd543bb929a
Faiyad <ahmedfaiyad7@gmail.com>
2025-10-04 21:31:12 -0400

    audio

--- server/app.py
+++ server/app.py
@@ -1,9 +1,11 @@
 from user.user import user
+from audio.audio import audio
 from pharma.pharma import pharma
 from order.order import order
 from flask import Flask, jsonify
 
 app = Flask(__name__)
+app.register_blueprint(audio, url_prefix="/audio")
 app.register_blueprint(user, url_prefix="/user")
 app.register_blueprint(pharma, url_prefix="/pharma")
 app.register_blueprint(order, url_prefix="/order")
---
+++ server/audio/audio.py
@@ -0,0 +1,20 @@
+from flask import Blueprint, request
+
+
+audio = Blueprint("audio", __name__)
+
+
+@audio.route("/")
+def index():
+    return "hello audio"
+
+
+@audio.route('/upload_audio', methods=['POST'])
+def upload_audio():
+    if 'audio' not in request.files:
+        return "No audio file part", 400
+
+    file = request.files['audio']
+    # Save it to disk
+    file.save("uploaded_audio.wav")  # or file.filename if you prefer
+    return "Audio received and saved!"
