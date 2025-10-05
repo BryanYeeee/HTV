de692c65837b294c79407176e1f1ab62cc8cfa4c
Patbob123 <flyingbikeddd@gmail.com>
2025-10-05 02:36:33 -0400

    sign up

--- frontend/src/app/home/page.js
+++ frontend/src/app/home/page.js
@@ -1,27 +1,37 @@
 'use client'
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import ClientPort from '@/portals/clientPort'
 import PharmPort from '@/portals/pharmPort'
 import { ThpaceGL } from 'thpace'
+import Cookies from 'js-cookie';
 
 const Home = () => {
   const canvasRef = useRef(null)
 
+  const [type, setType] = useState(null);
+
+  useEffect(() => {
+    const storedType = Cookies.get("type");
+    setType(storedType);
+  }, []);
+
+  console.log(type)
+
   useEffect(() => {
     if (!canvasRef.current) return
     const settings = {
-      // colors: ['white', '#c2d9ff', 'white'],
-      colors: ['white', '#aef2ae', 'white'],
+      colors: (type == "admin") ?  ['white', '#aef2ae', 'white'] : ['white', '#c2d9ff', 'white'],
       triangleSize: 200,
       pointAnimationSpeed: 5000
     }
 
     ThpaceGL.create(canvasRef.current, settings)
-    // if(admin)
-    document.documentElement.style.setProperty('--fore3', '#4caf6880')
-    document.documentElement.style.setProperty('--fore4', 'rgba(76, 175, 80, 1)')
-    document.documentElement.style.setProperty('--accent1', '#4caf6835')
-    document.documentElement.style.setProperty('--accent2', 'rgba(76, 175, 80, 0.35)')
+    if (type=="admin") {
+      document.documentElement.style.setProperty('--fore3', '#4caf6880')
+      document.documentElement.style.setProperty('--fore4', 'rgba(76, 175, 80, 1)')
+      document.documentElement.style.setProperty('--accent1', '#4caf6835')
+      document.documentElement.style.setProperty('--accent2', 'rgba(76, 175, 80, 0.35)')
+    }
   }, [])
 
   return (
@@ -32,7 +42,8 @@ const Home = () => {
         className='fixed top-0 left-0 w-full h-full z-0'
       />
       <div className='relative z-10'>
-        <PharmPort />
+        {type == "admin" ? (<PharmPort />) : (<ClientPort />)}
+        
         {/* <PharmPort /> if you need it */}
       </div>
     </>
